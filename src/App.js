4763f42783b3b9c56c566b1486df9d9d4d973154
Rafael_Rache <rafaelmarquesrache@gmail.com>
2020-04-22 12:12:55 -0300

    Reset website

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
-import Header from './components/Header/index'
-import Hero from './components/Hero/index'
-import About from './components/About/index'
 
 export default function App() {
   return (
-    <div>
-      <Header />
-      <Hero />
-      <About />
-    </div>
+    <section>
+
+    </section>
   );
 }
