f88b4d385dd74af2e46f3f761458507ce6812c77
Rafael_Rache <rafaelmarquesrache@gmail.com>
2020-04-23 11:45:46 -0300

    Add open and close code

--- src/components/Number_row/index.js
+++ src/components/Number_row/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 import "./style.css"
 
-export default function Number_Row () {
+export default function Number_Row (props) {
 
     var limit_number = Math.ceil(window.innerHeight / 10)
     var list = [];
+    var flag = false
     for (var i = 1; i < limit_number +1; i++) {
+        if (props.statusHide === true && i >3 && flag === false) {
+          limit_number= limit_number+25;
+          i=i+25;
+          flag = true
+        }
         list.push(i.toString());
     }
 
