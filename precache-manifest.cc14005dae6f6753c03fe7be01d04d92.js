453a8ff89a10ee78561d8771953349b7ee88f60c
Rafael_Rache <rafaelmarquesrache@gmail.com>
2020-04-27 10:07:49 -0300

    Updates

---
+++ precache-manifest.cc14005dae6f6753c03fe7be01d04d92.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac0e0853ce2fdcc56f6915219236bd5",
+    "url": "/WebSite/index.html"
+  },
+  {
+    "revision": "813f004dc249d2205f77",
+    "url": "/WebSite/static/css/main.3bb5541d.chunk.css"
+  },
+  {
+    "revision": "b1226161e9cba58b6a79",
+    "url": "/WebSite/static/js/2.36ef449a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WebSite/static/js/2.36ef449a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "813f004dc249d2205f77",
+    "url": "/WebSite/static/js/main.f11264e7.chunk.js"
+  },
+  {
+    "revision": "d9f210fa7382c8096070",
+    "url": "/WebSite/static/js/runtime-main.c7ff71b6.js"
+  },
+  {
+    "revision": "2d34307cf18c6d178cfc3b58d1656cce",
+    "url": "/WebSite/static/media/app_Init.2d34307c.png"
+  },
+  {
+    "revision": "fad06768662d039d34b6fad9b4c74616",
+    "url": "/WebSite/static/media/app_Logo_Horizontal.fad06768.png"
+  },
+  {
+    "revision": "55be92f97431cb2e9094243146545d75",
+    "url": "/WebSite/static/media/app_Opened.55be92f9.png"
+  },
+  {
+    "revision": "b6fd2b5c2120d258bacad7f2a1628a45",
+    "url": "/WebSite/static/media/logo.b6fd2b5c.png"
+  },
+  {
+    "revision": "de12181fddd64e817b5420b613d60b40",
+    "url": "/WebSite/static/media/mountain.de12181f.png"
+  },
+  {
+    "revision": "b6feda17b108dc752dbd13d026d0fc62",
+    "url": "/WebSite/static/media/react.b6feda17.png"
+  }
+]);
